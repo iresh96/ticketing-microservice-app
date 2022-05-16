7aad5e100d4357a5723e612d296eba601af1cac9
iresh96 <ireshmadushanka03@gmail.com>
2022-05-16 15:19:45 +0530

    order created ,cancelled listners finished with tests

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -6,6 +6,7 @@ import {
   validateRequest,
   requireAuth,
   NotAuthorizedError,
+  BadRequestError,
 } from "@sim96tickets/common";
 import { TicketUpdatedPublisher } from "../events/publishers/ticket-updated-publisher";
 import { natsWrapper } from "../nats-wrapper";
@@ -27,6 +28,10 @@ router.put(
 
     if (!ticket) {
       throw new NotFoundError();
+    }
+
+    if (ticket.orderId) {
+      throw new BadRequestError("Cannot edit reserved Ticket.");
     }
 
     if (ticket.userId !== req.currentUser!.id) {

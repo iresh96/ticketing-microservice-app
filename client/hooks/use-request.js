39c9e89b82845b309ced8f6686bffdae19ba2e84
iresh96 <ireshmadushanka03@gmail.com>
2022-05-21 12:49:44 +0530

    stripe payment setup fromnt end

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -4,9 +4,9 @@ import axios from "axios";
 const useRequest = ({ url, method, body, onSuccess }) => {
   const [errors, setErrors] = useState(null);
 
-  const doRequest = async () => {
+  const doRequest = async (props = {}) => {
     try {
-      const response = await axios[method](url, body);
+      const response = await axios[method](url, { ...body, ...props });
       setErrors(null);
       if (onSuccess) {
         onSuccess(response.data);
